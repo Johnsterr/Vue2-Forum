76af3dc4e749a16c0827b5efef9e75b20920146d
Evgeniy Pashko <evgpashko92@gmail.com>
2022-02-15 12:05:03 +0700

    Install firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,23 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import AppDate from "@/components/AppDate";
+import {initializeApp} from "firebase/app";
+import {getDatabase} from "firebase/database";
+import {getAuth} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const firebaseDatabase = getDatabase(firebaseApp);
+const firebaseAuth = getAuth(firebaseApp);
 
 Vue.component("AppDate", AppDate);
 
