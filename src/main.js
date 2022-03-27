ba57506e01b56b918499f9c6b5c9fdd04eec6cba
Evgeniy Pashko <evgpashko92@gmail.com>
2022-03-27 23:48:36 +0700

    Update dependencies, refactor firebase config

--- src/main.js
+++ src/main.js
@@ -6,16 +6,7 @@ import AppDate from "@/components/AppDate";
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-  databaseURL: process.env.VUE_APP_DATABASE_URL,
-  projectId: process.env.VUE_APP_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_APP_ID,
-};
+import firebaseConfig from "../config/firebase.js";
 
 export const firebaseApp = initializeApp(firebaseConfig);
 export const firebaseDatabase = getDatabase(firebaseApp);
